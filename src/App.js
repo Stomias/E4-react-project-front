9eefd71585b058732e5b74f41abba293ce2c8f37
zora-bdj <zora.berdji@estiam.com>
2021-12-12 20:47:42 +0100

    formulaire création training

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Connexion from './templates/connexion/Connexion';
 import Header from './templates/header/Header';
 import Footer from './templates/footer/Footer';
 // import TrainingList from './templates/trainingList/TrainingList';
+import TrainingCreation from './templates/trainingCreation/TrainingCreation';
 
 
 const App = () => {
   return (
     <div className="form-style">
       <Header />
-      <Connexion />
+      <TrainingCreation />
       <Footer />
     </div>
 
